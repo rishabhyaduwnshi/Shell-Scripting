4b76953cd3b1042c36e32c1c97436d87bcbe9234
Rishabh Yadav <1998rishabh31@gmail.com>
2024-05-25 20:09:29 +0530

    Create taking_input_from_user_8.sh

---
+++ taking_input_from_user_8.sh
@@ -0,0 +1,10 @@
+#1/bin/bash
+
+#taking input from user
+echo "Please enter your first name "
+read first_name
+echo "Your first name is : $first_name"
+
+#other way of taking input
+read -p "Enter your last name " last_name
+echo "Your last name is : last_name"
