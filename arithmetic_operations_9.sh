5d7eff002fdab4da55a29bde3809e341e6caae17
Rishabh Yadav <1998rishabh31@gmail.com>
2024-05-25 20:18:17 +0530

    Create arithmetic_operations_9.sh

---
+++ arithmetic_operations_9.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#arithmetic_operations by two different ways
+
+x=10
+y=20
+
+#addition
+let addition=x+y
+echo $addition
+
+#multiplication
+multiplication=$((x*y))
+echo $multiplication
