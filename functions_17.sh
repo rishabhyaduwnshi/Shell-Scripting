07b30093586a549c50dd6b6a5d623ecd4c891872
Rishabh Yadav <1998rishabh31@gmail.com>
2024-05-26 10:09:50 +0530

    Create functions_17.sh

---
+++ functions_17.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#first method
+function print_hi
+{
+    echo "Hi..."
+}
+
+print_hi
+print_hi
+
+
+#second method
+print_hello()
+{
+    echo "Hello..."
+}
+
+print_hello
+print_hello
